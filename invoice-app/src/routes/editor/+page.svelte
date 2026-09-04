dc4004ce33334809b7ead1f6de7176b9a5b45b49
Fahri rijalul ihsan <fahririjalulisan28@gmail.com>
2026-09-04 17:34:46 +0700

    setengah

--- invoice-app/src/routes/editor/+page.svelte
+++ invoice-app/src/routes/editor/+page.svelte
@@ -33,9 +33,7 @@
   import InvoiceBrutalistBracket from '$lib/components/invoices/InvoiceBrutalistBracket.svelte';
   import InvoiceEditorialMerah from '$lib/components/invoices/InvoiceEditorialMerah.svelte';
   import InvoiceSidebarBiru from '$lib/components/invoices/InvoiceSidebarBiru.svelte';
-  import Invoice33DarkWave from '$lib/components/invoices/Invoice33DarkWave.svelte';
-  import Invoice34ModernBlue from '$lib/components/invoices/Invoice34ModernBlue.svelte';
-  import Invoice35ElegantPurple from '$lib/components/invoices/Invoice35ElegantPurple.svelte';
+  
 
   import { page } from '$app/state';
   import { goto } from '$app/navigation';
@@ -88,9 +86,6 @@
     { name: () => `30. ${lang.t('tpl30_name')}`, component: InvoiceBrutalistBracket },
    { name: () => `31. ${lang.t('tpl31_name')}`, component: InvoiceEditorialMerah },
     { name: () => `32. ${lang.t('tpl32_name')}`, component: InvoiceSidebarBiru },
-    { name: () => `33. Dark Wave`, component: Invoice33DarkWave },
-    { name: () => `34. Modern Blue`, component: Invoice34ModernBlue },
-    { name: () => `35. Elegant Purple`, component: Invoice35ElegantPurple }
   ];
 
   let selected = $state(
