de3c0ccd222aacc2bbbc0f2474ccadf8fec024d4
Hype <farel>
2026-09-06 14:36:38 +0700

    deploy semua online

--- invoice-app/src/lib/authStore.svelte.js
+++ invoice-app/src/lib/authStore.svelte.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:8800/api' || 'http://localhost:8000/api';
+const API_BASE = 'https://app-inv.xo.je';
 
 
 
--- invoice-app/src/lib/invoiceStore.svelte.js
+++ invoice-app/src/lib/invoiceStore.svelte.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:8800/api';
+const API_BASE = 'https://app-inv.xo.je';
 
 function getToken() {
   return localStorage.getItem('auth_token');
